8b864f6ab3a249465c6e59f1f8602706c2414249
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2019-05-10 13:37:25 +0930

    Create decrypt_and_fastq_dump.sh

---
+++ decrypt_and_fastq_dump.sh
@@ -0,0 +1,40 @@
+#!/bin/bash                                                                                                                                                                                                                                                                    
+
+#SBATCH -A robinson                                                                                                                                                                                                                                                            
+#SBATCH -p batch                                                                                                                                                                                                                                                               
+#SBATCH -n 20                                                                                                                                                                                                                                                                  
+#SBATCH -N 2                                                                                                                                                                                                                                                                   
+#SBATCH --time=03-00:00                                                                                                                                                                                                                                                        
+#SBATCH --mem=50GB                                                                                                                                                                                                                                                             
+
+# notification configuration                                                                                                                                                                                                                                                   
+#SBATCH --mail-type=END                                                                                                                                                                                                                                                        
+#SBATCH --mail-type=FAIL                                                                                                                                                                                                                                                       
+#SBATCH --mail-user=urwah.nawaz@adelaide.edu.au                                                                                                                                                                                                                       
+
+export WD="/fast/users/a1654797/gtex_data"
+
+touch $WD/out/decrypt
+touch $WD/out/fastq_dump
+export DIR="/fast/users/a1654797/dbGaP/sra"
+
+if [ ! -d "$DIR" ]; then
+    echo "Wrong directory!"
+    exit 1
+fi
+
+cd $DIR
+
+echo $(date) >> $WD/out/decrypt.log
+
+#check that all files are decrypted                                                                                                                                                                                                                                            
+arr=(./*)
+
+for f in "${arr[@]}"; do
+    echo "$f"
+    vdb-decrypt ./$f &>>$WD/out/decrypt.log
+done
+
+#get the fastq for all the sra files                                                                                                                                                                                                                                          
+
+fastq-dump --gzip --split-files --unaligned ./*.sra &>>$WD/out/fastq_dump.log
