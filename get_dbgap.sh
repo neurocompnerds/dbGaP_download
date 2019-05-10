794f57b13e1355b542e95ee48e84a94958bd2380
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2019-05-10 11:40:16 +0930

    Update get_dbgap.sh

--- get_dbgap.sh
+++ get_dbgap.sh
@@ -5,26 +5,26 @@
 #SBATCH -n 18
 #SBATCH -N 1
 #SBATCH --time=03-00:00
-#SBATCH --mem=120GB
+#SBATCH --mem=60GB
 
 # notification configuration
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=urwah.nawaz@student.adelaide.edu.au
+#SBATCH --mail-user=urwah.nawaz@adelaide.edu.au ### change to your email
 
 ### modules needed 
 module load  Aspera-Connect/3.8.3
 module load SRA-Toolkit/2.8.2-1-centos_linux64
 
+# repository encryption key
+export KEY="prj_21432_D23645.ngc"  ## change to your repository key
 
-#encryption key, change to whatever yours is
-export KEY="prj_21432_D23645.ngc"
+#cart file
+export CART="cart_prj21432_201905062302.krt"  # change to your cart file
 
-#cart downloaded from dbgap "run browser" container list of files to download
-
-export CART="cart_prj21432_201905062302.krt "
-
-export LOGDIR="/fast/users/a1654797/gtex_data"
+# log dir
+export LOG="/fast/users/a1654797/gtex_data" 
+touch $LOG/dbgap_download.log
 
 export DIR="/fast/users/a1654797/dbGaP"
 
@@ -32,14 +32,15 @@ export DIR="/fast/users/a1654797/dbGaP"
      mkdir -p $DIR
  fi
 
+# scripts need to be ran from the directory where the encryption key will be imported
 cd $DIR
 
 #Downloading Gtex data
 
 # importing the key repository 
-vdb-config --import $KEY ./ &>$LOGDIR/out/dbgap_download.log
+vdb-config --import $KEY ./ &>$LOG/out/dbgap_download.log
 
 # all codes must be executed from the key repository to get the data
 
 prefetch ../$CART \
-&>>$LOGDIR/out/dbgap_download_2.log
+&>>$LOG/out/dbgap_download.log
